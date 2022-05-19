e62662588eef3a99968875020d88cf4a30974e9c
enrojass04 <enrojass04@example.com>
2022-05-19 15:34:05 -0500

    version2

--- js/index.js
+++ js/index.js
@@ -1,22 +1,35 @@
-const parrafo1 = document.querySelector("#par1");
-console.log(parrafo1);
-
-
-const cambiarColor = (id) => {
-    let element = document.querySelector(`.${id}`);
-    element.classList.add("estilo2");
-}
-
 const cambiarFondo = (id) => {
-    let element = document.getElementById("body");
+    let element = document.getElementById("body");    
     element.style.backgroundColor = id.value;
+    localStorage.setItem('color', id.value);
+
+    /* let element2 = document.getElementById("tema1");    
+    element2.style.backgroundColor = id.value;
+    localStorage.setItem('color', id.value); */
+
+    let element3 = document.getElementById("tema2");    
+    element3.style.backgroundColor = id.value;
     localStorage.setItem('color', id.value);
 }
 
+
+
 const cargarColor = () => {
-    const color = localStorage.getItem("color");
-    if (color) {
+    const color1 = localStorage.getItem("color");
+    if (color1) {
         let element = document.getElementById("body");
-        element.style.backgroundColor = color;
+        element.style.backgroundColor = color1;
     }
+
+    const color3 = localStorage.getItem("color");
+    if (color3) {
+        let element3 = document.getElementById("tema2");
+        element3.style.backgroundColor = color3;
+    }
+
+    /* const color2 = localStorage.getItem("color");
+    if (color2) {
+        let element2 = document.getElementById("tema1");
+        element2.style.backgroundColor = color1;
+    } */
 }
